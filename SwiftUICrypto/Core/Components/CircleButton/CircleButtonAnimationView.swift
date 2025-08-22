124d5572a1cd6b099de07d95253aed66edd331ad
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-08-22 23:22:38 +0200

    Creating HomeView and a navigation header

---
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonAnimationView.swift
+//  SwiftUICrypto
+//
+//  Created by Roger Florat Gutierrez on 22/08/25.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
---
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  SwiftUICrypto
+//
+//  Created by Roger Florat Gutierrez on 22/08/25.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
