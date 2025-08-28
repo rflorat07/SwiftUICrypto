1fd4dcdd7d2e43314e033d6b0eff95ff8b89fd73
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-08-28 10:53:08 +0200

    Download and display live market data from API

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,19 +9,15 @@ import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject {
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: 25.24),
-        StatisticModel(title: "Market Cap", value: "$12.5Bn"),
-        StatisticModel(title: "Market Cap", value: "$12.5Bn"),
-        StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: -25.24),
-    ]
-    
-    @Published var allCoints: [CoinModel] = []
-    @Published var portfolioCoints: [CoinModel] = []
     
     @Published var searchText: String = ""
+   
+    @Published var allCoints: [CoinModel] = []
+    @Published var portfolioCoints: [CoinModel] = []
+    @Published var statistics: [StatisticModel] = []
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellable = Set<AnyCancellable>()
     
     init () {
@@ -32,11 +28,19 @@ class HomeViewModel: ObservableObject {
         
         // Update allCoints
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoints)
             .sink{ [weak self] (returnedCoins) in
                     self?.allCoints = returnedCoins
+            }
+            .store(in: &cancellable)
+        
+        // update marketData
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
             }
             .store(in: &cancellable)
     }
@@ -53,6 +57,27 @@ class HomeViewModel: ObservableObject {
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        
+        guard let data = marketDataModel else { return stats }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        
+        let volumen = StatisticModel(title: "24h Volumen", value: data.volumen)
+        
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        
+        let portfolioValue = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+
+        
+        stats.append(contentsOf: [
+            marketCap, volumen, btcDominance, portfolioValue
+        ])
+        
+        return stats
     }
 
 }
