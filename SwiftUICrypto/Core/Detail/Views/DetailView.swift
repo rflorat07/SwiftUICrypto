ae46d1cbf1888c3c5c91c084a12ef1d4177c3e31
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-09-05 12:26:06 +0200

    Build an expandable / collapsable description section

--- SwiftUICrypto/Core/Detail/Views/DetailView.swift
+++ SwiftUICrypto/Core/Detail/Views/DetailView.swift
@@ -23,6 +23,8 @@ struct DetailView: View {
     
     @StateObject var vm: DetailViewModel
     
+    @State private var showFullDescription: Bool = false
+    
     private let columns : [GridItem] = Array(repeating: GridItem(.flexible(minimum: 100)), count: 2)
     
     private let spacing: CGFloat = 30
@@ -41,11 +43,15 @@ struct DetailView: View {
                 VStack(spacing: 20) {
                     overviewTitle
                     Divider()
+                          
+                    descriptionSection
                     overviewGrid
                     
                     additionalTitle
                     Divider()
                     additionalGrid
+                    
+                    websiteSection
                 }
                 .padding()
                 
@@ -99,6 +105,34 @@ extension DetailView {
             .frame(maxWidth: .infinity, alignment: .leading)
     }
     
+    private var descriptionSection: some View {
+        ZStack {
+            if let coinDescription = vm.coinDescription, !coinDescription.isEmpty {
+                VStack(alignment: .leading) {
+                    Text(coinDescription)
+                        .lineLimit(showFullDescription ? nil : 3)
+                        .font(.callout)
+                        .foregroundColor(.theme.secondaryText)
+                    
+                    Button {
+                        withAnimation(.easeInOut) {
+                            showFullDescription.toggle()
+                        }
+                    } label: {
+                        Text(showFullDescription ? "Show less" : "...more")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 4)
+                    }
+                    .accentColor(.blue)
+
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+    
+    
     private var overviewGrid: some View {
         LazyVGrid(
             columns: columns,
@@ -123,5 +157,24 @@ extension DetailView {
                 StatisticView(stat: stat)
             }
         }
+    }
+    
+    private var websiteSection: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            if let websiteString = vm.websiteURL,
+               let url = URL(string: websiteString) {
+                
+                Link("Website", destination: url)
+            }
+            
+            if let redditString = vm.redditURL,
+               let url = URL(string: redditString) {
+                
+                Link("Reddit", destination: url)
+            }
+        }
+        .accentColor(.blue)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.headline)
     }
 }
